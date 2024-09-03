4ceca13467363be499d363d5955cc0af3a270a34
hansnguyennn <hansnguyenctm@gmail.com>
2024-09-02 21:42:37 -0400

    Hi, I made a little change to send to my friend

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Dear Bảo Hân, have a nice day ạ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
